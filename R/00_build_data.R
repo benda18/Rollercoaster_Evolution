6edfd7abf431bfb5a2a5d26e4db120159b752563
Tim Bender <bendertj@gmail.com>
2023-09-18 11:15:23 -0400

    Add files via upload
    setup working directory variables in 00_build_data.R

--- R/00_build_data.R
+++ R/00_build_data.R
@@ -6,4 +6,33 @@ library(rvest)
 library(xml2)
 library(glue)
 library(lubridate)
-library(ggplot2)
+#library(ggplot2)
+library(readr)
+
+
+rm(list=ls());cat('\f');gc()
+
+# FUNS ----
+
+# DIRS ----
+wd        <- list()
+wd$data   <- "C:/Users/bende/Documents/R/play/rollercoaster_evolution/data"
+wd$R      <- "C:/Users/bende/Documents/R/play/rollercoaster_evolution/R"
+wd$output <- "C:/Users/bende/Documents/R/play/rollercoaster_evolution/output"
+wd$home   <- "C:/Users/bende/Documents/R/play/rollercoaster_evolution"
+
+
+# VARS ----
+
+# BUILD ----
+# Build selected_parks.csv
+
+# Build crosswalk_pr.csv
+
+# Build park_inventory.csv
+
+# Build ride_inventory.csv
+
+# Build ride_specs.csv
+
+# Build ride_status.csv
