b170726907ca69cd1cc5eb0fcb966f0ad2810af3
Tim Bender <bendertj@gmail.com>
2023-09-22 14:18:35 -0400

    periodic backup

--- R/10_cleanddata_explore.R
+++ R/10_cleanddata_explore.R
@@ -8,6 +8,40 @@ library(lubridate)
 rm(list=ls());cat('\f');gc()
 
 # FUNS ----
+
+
+# pi <- read_csv("park_inventory.csv") %>%
+#   .[.$park_name == "kings_island",] %>%
+#   .[!.$ride_status == "under_constr",]
+# 
+# pi <- ride_specs_url_years %>%
+#   left_join(.,
+#             summarise(group_by_all(park_inventory[,c("park_name", "ride_url", "ride_name")]))) %>%
+#   .[.$park_name == "kings_island",] %>%
+#   .[!.$ride_status == "under_constr",]
+
+build_the_year2 <- function(yr1 = 1992, 
+                            df1 = pi){
+  require(data.table)
+  require(dplyr)
+  bw.out <- NULL
+  for(i in 1:nrow(df1)){
+    bw.out <- c(bw.out, 
+                    data.table::between(x     = yr1, 
+                                        lower = df1$yro_best[i],
+                                        upper = df1$yrc_best[i], 
+                                        NAbounds = NA))
+  }
+  bw.out <- ifelse(is.na(bw.out), F, bw.out)
+  
+  df1$active_year <- yr1
+  out <- df1[!colnames(df1) %in% c("yro_best", "yrc_best")] %>%
+    .[bw.out,] 
+ 
+  return(out)
+}
+
+
 build_the_year <- function(yr, df.rides){
   df.rides$yrc_best[is.na(df.rides$yrc_best) & 
                       df.rides$ride_status != "under_construction"] <- year(Sys.Date())
@@ -29,6 +63,10 @@ build_the_year <- function(yr, df.rides){
                        year_active = yr)
   return(out.df)
 }
+
+
+
+
 
 # DIRS ----
 wd        <- list()
@@ -63,7 +101,7 @@ ride_specs_url_years <- ride_specs[,c("ride_url", "height.ft", "length.ft", "spe
             park_inventory[,c("ride_url", "yro_best", "yrc_best", 
                               "type", "ride_status")]) %>%
   # remove rides under construction
-  .[.$ride_status != "under_constr",]
+  .[.$ride_status != "under_constr",] 
 
 # set yrc_best for rides still operating----
 ride_specs_url_years$yrc_best[ride_specs_url_years$ride_status %in% 
@@ -105,7 +143,7 @@ rcby.bu <- rcby
 rcby <- rcby.bu
 
 rcby.summary <- rcby %>%
-  .[.$year_active >= 1920,] %>%
+  #.[.$year_active >= 1920,] %>%
   group_by(year = year_active) %>%
   summarise(n_rides = n_distinct(ride_url), 
             n_parks = n_distinct(park_url), 
@@ -113,4 +151,74 @@ rcby.summary <- rcby %>%
             max_length = max(length.ft, na.rm = T), 
             max_speed = max(speed.mph, na.rm = T)) 
 
-plot(rcby)
+
+breakpts <- data.frame(bp_year = c(1972, 1979, 2000, 2009), 
+                       ride = c("racer", "beast", "son_of_beast", "son_of_beast"))
+
+
+ggplot() + 
+  geom_line(data = rcby.summary, 
+            aes(x = year, y = max_height), 
+            linewidth = 1.2)+
+  scale_y_continuous(limits = c(0,NA))+
+  labs(title = "Max Height")+
+  scale_x_continuous(limits = c(1880,2030), 
+                     breaks = seq(1800,3000,by= 20))+
+  geom_vline(aes(xintercept = breakpts$bp_year), 
+             linetype = 2232, color = "black")
+
+
+ggplot() + 
+  geom_line(data = rcby.summary, 
+            aes(x = year, y = max_length), 
+            linewidth = 1.2)+
+  scale_y_continuous(limits = c(0,NA))+
+  labs(title = "Max Length")+
+  scale_x_continuous(limits = c(1880,2030), 
+                     breaks = seq(1800,3000,by= 20))+
+  geom_vline(aes(xintercept = breakpts$bp_year), 
+             linetype = 2232, color = "black")
+
+
+ggplot() + 
+  geom_line(data = rcby.summary, 
+            aes(x = year, y = max_speed), 
+            linewidth = 1.2)+
+  scale_y_continuous(limits = c(0,NA))+
+  labs(title = "Max Speed")+
+  scale_x_continuous(limits = c(1880,2030), 
+                     breaks = seq(1800,3000,by= 20))+
+  geom_vline(aes(xintercept = breakpts$bp_year), 
+             linetype = 2232, color = "black")
+
+
+
+# choose a park----
+
+rcby
+jb_ridestuff <- park_inventory[,c("ride_name", 
+                                  #"type",
+                                  "design", "park_name", 
+                                  #"ride_status", 
+                                  "ride_url", "park_url")] %>%
+  group_by_all() %>%
+  summarise()
+
+master <- left_join(rcby, jb_ridestuff)
+
+
+a.park <- "kings_island"
+
+master %>%
+  .[.$park_name == a.park,] %>%
+  group_by(year = year_active, 
+           #ride_url, ride_name, 
+           #type, 
+           design,
+           park_url, 
+           park_name) %>%
+  summarise(n_rides = n_distinct(ride_url)) %>%
+  ggplot(data = ., 
+       aes(x = year, y = n_rides, fill = design)) + 
+  geom_area(color = "black", 
+            position = "stack")
